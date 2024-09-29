88c78babad9159649dd63efa8cd66991eb06a54e
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2024-09-29 19:02:29 +0400

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = index;
+  }
+  document.querySelector(".slides").style.transform = `translateX (-${
+    slideIndex * 100
+  }%)`;
+}
+
+function changeSlide(direction) {
+  showSlide(slideIndex + direction);
+}
+setInterval(() => {
+  changeSlide(1);
+}, 3000);
